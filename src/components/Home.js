79fc80a6fcb969346f94e133eec1da15b2949de8
Nina Mutty <ninamutty@gmail.com>
2017-01-18 21:48:44 -0800

    request a user when redirected

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Home = props => {
-  console.log(props);
-  console.log(props.params.user_id);
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.userID = props.params.user_id;
+    console.log(this.userID);
+    this.state = {user: {}}
+  }
 
-  var userID = props.params.user_id ;
+  getUser(userID) {
+    const BASE_URL = 'http://localhost:8080/'
 
-  console.log(userID);
-  return (
-    <div className="Home">
-      <h1> WELCOME TO HOME </h1>
-      <p> {userID} </p>
-    </div>
-  );
+    fetch(`${BASE_URL}api/users/${userID}`, {
+      accept: 'application/json',
+    }).then( (response) => {
+      return response.json();
+    }).then( (response) => {
+      this.setState({user: response.user})
+    }).catch(function(err) {
+      console.log(err);
+    });
+  }
+
+  checkUser() {
+    if (this.state.user.name === undefined) {
+      return this.getUser(this.userID)
+    }
+  }
+
+  mapSubscriptions() {
+    let subscriptions = this.state.user.subscriptions;
+    let subscriptionList = subscriptions
+  }
+
+  render() {
+    return (
+      <div className="Home">
+        { this.checkUser() }
+        <h1> Welcome, {this.state.user.name} </h1>
+      </div>
+    );
+  }
 }
 
 export default Home;
+
+
+
+// const Home = props => {
+//   console.log(props);
+//   console.log(props.params.user_id);
+//
+//   var userID = props.params.user_id ;
+//
+//   console.log(userID);
+//   return (
+//     <div className="Home">
+//       <h1> WELCOME TO HOME </h1>
+//       <p> {userID} </p>
+//     </div>
+//   );
+// }
