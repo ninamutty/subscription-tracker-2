cef744862e324085acdca10c229b6ecbb6f2379b
Nina Mutty <ninamutty@gmail.com>
2017-01-25 09:45:29 -0800

    fixed login-page styling

--- src/components/Verify.js
+++ src/components/Verify.js
@@ -12,7 +12,7 @@ class Verify extends Component {
     return (
       <div className="main-content verify">
         <h2>Welcome! </h2>
-        <h4>With Subscription Tracker, You Can Easily Track All of Your Subscriptions In One Place </h4>
+        <h4>With Subscribed, You Can Easily Track All of Your Subscriptions In One Place </h4>
 
         <div className="forms row">
           <SignUp />
